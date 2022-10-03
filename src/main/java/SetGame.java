41ecf036a4d62746a664b6db6a5111d80f9b0f8d
yaping0816 <yaping0816@gmail.com>
2022-10-03 13:50:08 -0700

    Added a timer functionality

---
+++ src/main/java/SetGame.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class SetGame {
+
+  public static void main(String[] args) {
+    long start = System.currentTimeMillis();
+    long end = start+6*1000;
+
+    while(System.currentTimeMillis() < end){
+
+      System.out.println("Our game displays here");
+
+    }
+    System.out.println("You find x sets!!!");
+  }
+
+
+
+
+}
