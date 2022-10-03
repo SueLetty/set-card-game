f01d446e4ad87aeff05cd132e7862101e84f2437
yaping0816 <yaping0816@gmail.com>
2022-10-03 10:01:10 -0700

    Completed Number and Shape enum

--- src/main/java/Number.java
+++ src/main/java/Number.java
@@ -1,5 +1,11 @@
 public enum Number {
-  
-  ONE, TWO,THREE
+
+  ONE, TWO,THREE;
+
+  @Override
+  public String toString() {
+    String name = name();
+    return name.charAt(0) + name.substring(1).toLowerCase();
+  }
 
 }
--- src/main/java/Shape.java
+++ src/main/java/Shape.java
@@ -1,4 +1,10 @@
 public enum Shape {
 
-  OVAL, SQUIGGLES, DIAMOND
+  OVAL, SQUIGGLES, DIAMOND;
+
+  @Override
+  public String toString() {
+    String name = name();
+    return name.charAt(0) + name.substring(1).toLowerCase();
+  }
 }
