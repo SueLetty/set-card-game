6e460419baa6bd3bcac01b284a429b1eb85d5d38
yaping0816 <yaping0816@gmail.com>
2022-10-03 14:38:38 -0700

    Completed most tests for Card

--- src/main/java/Shape.java
+++ src/main/java/Shape.java
@@ -5,6 +5,6 @@ public enum Shape {
   @Override
   public String toString() {
     String name = name();
-    return name.charAt(0) + name.substring(1).toLowerCase();
+    return name.charAt(0) + name.substring(1).toLowerCase(); //Oval, Squiggles, Diamond
   }
 }
---
+++ src/test/java/CardTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CardTest {
+  private Card card;
+
+  @BeforeEach
+  public void init(){
+   card = new Card(Number.ONE, Shape.DIAMOND,Color.GREEN, Shading.OUTLINED);
+
+  }
+
+
+  @Test
+  void getShape() {
+    assertTrue(card.getShape()== Shape.DIAMOND);
+  }
+
+
+  @Test
+  void getNumber() {
+    assertEquals("One", card.getNumber().toString());
+  }
+
+  @Test
+  void getColor() {
+    assertEquals("Green", card.getColor().toString());
+  }
+
+  @Test
+  void getShading() {
+    assertEquals("Outlined", card.getShading().toString());
+  }
+
+  @Test
+  void testHashCode() {
+    System.out.println(card.hashCode());
+  }
+
+  @Test
+  void testEquals() {
+    Card other1 = new Card(Number.ONE, Shape.DIAMOND,Color.GREEN, Shading.OUTLINED);
+    assertEquals(card, other1);
+    Card other2 = new Card(Number.THREE, Shape.DIAMOND, Color.GREEN, Shading.OUTLINED);
+    assertNotEquals(card, other2);
+
+  }
+
+  @Test
+  void testToString() {
+  }
+}
