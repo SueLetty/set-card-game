8a629c721490965e071ddc458e10bb85a6b0a53e
SueLetty <xiuxiang1129@gmail.com>
2022-10-06 14:13:16 -0700

    Add Java doc

---
+++ docs/api/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"Deck"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Number"},{"p":"<Unnamed>","l":"SetGame"},{"p":"<Unnamed>","l":"Shading"}]
